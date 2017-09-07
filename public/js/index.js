47ba0ca9246986f5b1d24991f9514d38d5504f59
unknown <avers0000@gmail.com>
2017-09-07 19:44:12 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to server');
 
-	socket.emit('createMessage', {
-		from: 'Dart Vader',
-		text: 'Hey. This is Alexey'
-	});
 });
 
 socket.on('disconnect', function () {
